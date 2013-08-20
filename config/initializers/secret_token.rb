8e04e68eb4573ef92bf51c7adeacfc0a60f74c43
Ronen Verdi <go7hic@gmail.com>
2013-08-20 19:05:52 -0400

    Initial commit
    Starting the guestbook app with the help of Ruby on Rails and AngularJS
    frameworks

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Guestbook::Application.config.secret_key_base = '6f9d245533be8e293f9dfdeb3a2db2538bd0f249ad8ff4b6d49510d2aa48df1e0c5e11446e1dc788249aa396357aaf547817936718b15a95590e613768757d68'
